3c9e087e8dbb2c77395cbbc3234c80704e76ecd2
W33dw3y <150056959+W33dw3y@users.noreply.github.com>
2024-05-23 23:11:19 +0200

    ProfileCard

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { RouterLink, RouterView } from 'vue-router/auto'
 import HeaderPage from './components/HeaderPage.vue';
 import FooterPage from './components/FooterPage.vue';
 import MoodCard from './components/MoodCard.vue';
+import ProfileCard from './components/ProfileCard.vue';
 
 import { moodList } from '@/data';
 
@@ -21,9 +22,10 @@ onErrorCaptured((err, instance, info) => {
     </Suspense>
   </RouterView> -->
   <main class="bg-lightBeige">
-    <section class="flex flex-col gap-4">
+    <!-- <section class="flex flex-col gap-4">
       <MoodCard v-for="mood in moodList" v-bind="mood" :key="mood"/>
-    </section>
+    </section> -->
+    <ProfileCard v-for="mood in moodList" v-bind="mood" :key="mood"/>
   </main>
   <FooterPage />
 </template>
---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import IconSmallSettings from './icons/IconSmallSettings.vue';
+import IconProfileSmall from './icons/IconProfileSmall.vue';
+import IconMoodBad from './icons/IconMoodBad.vue';
+import IconMoodMid from './icons/IconMoodMid.vue';
+import IconMoodGood from './icons/IconMoodGood.vue';
+
+import { moodList } from '@/data';
+
+const props = defineProps<{
+    pseudo: string;
+    date: string;
+    mood: string;
+}>();
+</script>
+
+
+<template>
+    <section class="flex justify-between items-center mx-6 p-4">
+        <div class="flex gap-1">
+            <IconProfileSmall/>
+            <span class="relative flex items-end *:absolute *:w-8 *:h-8 *:right-0">
+                <IconMoodGood v-if="props.mood === 'Bien'"/>
+                <IconMoodMid v-if="props.mood === 'Moyen'" />
+                <IconMoodBad v-if="props.mood === 'Mal'" />
+            </span>
+            <p class="self-start">{{ pseudo }}</p>
+        </div>
+        <div>
+            <IconSmallSettings class="h-6"/>
+        </div>
+    </section>
+</template>
