81d530f9f8716f16236042ca589b33e962d93577
W33dw3y <150056959+W33dw3y@users.noreply.github.com>
2024-05-27 08:28:06 +0200

    inputconnexion.card

---
+++ src/components/InputConnexion.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = withDefaults (
+    defineProps<{
+        text?: string
+        labelfor: string
+        type: string
+        name: string
+        id: string
+        placeholder: string
+    }>(),
+    {
+        type: 'text',
+    }
+)
+</script>
+
+<template>
+    <label :for="`${labelfor}`">{{ text }}</label>
+    <input :type="`${type}`" :name="`${name}`" :id="`${id}`" :placeholder="`${placeholder}`">
+</template>
