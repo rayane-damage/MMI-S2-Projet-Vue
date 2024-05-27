574474be09c012e241870cc201b2a223757b481d
jeremy roy <jeremy.roy@edu.univ-fcomte.fr>
2024-05-27 14:24:19 +0200

    debut settings

---
+++ src/pages/settings/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { isActive } from '@/components/HeaderPage.vue'
+// import IconInfo from './icons/IconInfo.vue'
+</script>
+
+<template>
+    <section class="bg-mainBlue">
+        <div>
+            <div>
+                <p>Informations du compte</p>
+                <IconInfo />
+            </div>
+            <RouterLink to="/Settings/ChangePassword">
+                <p>Changer le mot de passe</p>
+            </RouterLink>
+            <RouterLink to="/Settings/Notifications">
+                <p>Notifications</p>
+            </RouterLink>
+        </div>
+        <div>
+            <div>
+                <p>Déconnection</p>
+                <IconInfo />
+            </div>
+        </div>
+    </section>
+</template>
