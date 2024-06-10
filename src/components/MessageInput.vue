060f150f8558c45e230b56b226264e2f57eeb932
W33dw3y <150056959+W33dw3y@users.noreply.github.com>
2024-06-10 10:08:48 +0200

    commencement de la messagerie

---
+++ src/components/MessageInput.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { MessagesResponse } from '@/pocketbase-types';
+import { pb } from '@/backend';
+import { ref, inject } from 'vue';
+import type { Ref } from 'vue';
+// const props = defineProps<MessagesResponse>();
+const text = ref<string>('');
+const allMessagesByUsers = inject('allMessagesByUsers') as Ref<any[]>;
+
+const doSendMessage = async (message:string) => {
+    if (message) {
+        console.log('Sending message', text);
+        await pb.collection('messages').create({
+            message: message,
+            from: pb.authStore.model?.id,
+            to: "jgd9q7r3l6u7loo"
+        });
+        text.value = '';
+        allMessagesByUsers.value = await pb.collection('messages').getFullList({
+            filter: `from = '${pb.authStore.model?.id}' && to = "jgd9q7r3l6u7loo" || to = '${pb.authStore.model?.id}' && from = "jgd9q7r3l6u7loo" `,
+            expand : 'from && to'
+        });
+        
+    } else {
+        console.log('No message to send');
+        alert('No message to send');
+    }
+}
+
+</script>
+
+<template>
+    <section class="absolute bottom-[80px] w-full flex justify-center items-center">
+        <div class="w-full">
+            <input v-model="text" type="text" class="p-2 rounded-full py-4 w-full" placeholder="Message" />
+        </div>
+        <div class="bg-white rounded-full p-2">
+            <button @click="doSendMessage(text)">Envoyer</button>
+        </div>
+    </section>
+</template>
--- src/pages/messages/index.vue
+++ src/pages/messages/index.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import HeaderPage from '@/components/HeaderPage.vue';
 import ProfileCard from '@/components/ProfileCard.vue';
-import MessagesCard from '@/components/MessagesCard.vue';
+import DiscussionCardard from '@/components/DiscussionCard.vue';
+import MessageInput from '@/components/MessageInput.vue';
 
 import { pb } from '@/backend';
 import { isActive } from '@/components/HeaderPage.vue'
-import { getUserFriends } from '@/backend';
 import type { UsersResponse, MessagesResponse } from '@/pocketbase-types';
+import { ref, provide } from 'vue';
 
 const moodList = await pb.collection('mood').getFullList({
         filter : `user = '${pb.authStore.model?.id}'`,
@@ -17,25 +18,36 @@ if (isActive.value == false) {
     isActive.value = true;
 }
 
-// const userFriends: UsersResponse[] = await getUserFriends();
 const userFriends: UsersResponse[] = await pb.collection('users').getFullList({
     filter: `friends.username = '${pb.authStore.model?.username}'`,
     expand : 'friends'
 });
 
-const userMessages: MessagesResponse[]  = await pb.collection('messages').getFullList({
-    filter: `user = '${pb.authStore.model?.id}'`
+// const userMessages: MessagesResponse[]  = await pb.collection('messages').getFullList({
+//     filter: `user.id = '${pb.authStore.model?.id}'`,
+//     expand : 'user'
+// });
+
+const allMessages: MessagesResponse[] = await pb.collection('messages').getFullList({
+    filter: `from = '${pb.authStore.model?.id}' && to = "jgd9q7r3l6u7loo" || to = '${pb.authStore.model?.id}' && from = "jgd9q7r3l6u7loo" `,
+    expand : 'from && to'
 });
-console.log(pb.authStore.model?.id)
-console.log(userMessages);
+
+const allMessagesByUsers = ref(allMessages)
+
+provide('allMessagesByUsers', allMessagesByUsers);
+
+
 </script>
 
 <template>
     <HeaderPage active="Discussions" inactive="Amis" :currentMood="moodList[0].mood"/>
 
-    <section v-if="isActive === true">
-        <h1>lol</h1>
-        <MessagesCard v-for="message in userMessages" :key="message.id" v-bind="message" />
+    <section v-if="isActive === true" >
+        <div class="flex flex-col gap-2 p-4">
+            <DiscussionCardard v-for="message in allMessagesByUsers" :key="message.id" v-bind="message" />
+        </div>
+        <MessageInput />
     </section>
 
     <section v-if="isActive === false">
