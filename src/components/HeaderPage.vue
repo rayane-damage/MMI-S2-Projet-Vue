bdfe168f7c16999578b69a4f94640ce4071d970d
rayane-damage <150058299+rayane-damage@users.noreply.github.com>
2024-06-12 13:33:22 +0200

    header profile picture edit

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -33,7 +33,10 @@ export const isActive = ref(true);
                 <ul class="flex justify-between px-8 pt-4">
                     <li>
                         <RouterLink to="/profil">
-                             <ImgPb :record="currentUser" :filename="currentUser.avatar" alt="Photo de profil" class="w-10 h-10 first-letter:object-cover rounded-full"/>
+                             <ImgPb :record="currentUser" 
+                                    :filename="currentUser.avatar" 
+                                    alt="Photo de profil"
+                                    class="w-10 h-10 first-letter:object-cover rounded-full border-2 border-white"/>
                             <span class="relative flex items-end *:absolute *:w-4 *:h-4 *:right-0">
                                 <IconMoodGood v-if="props.currentMood === 'Bien'"/>
                                 <IconMoodMid v-if="props.currentMood === 'Moyen'"/>
