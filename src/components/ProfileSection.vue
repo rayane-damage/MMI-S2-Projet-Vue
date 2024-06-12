d8d3c794a1baab56f267c05675c05328e6f208e3
W33dw3y <150056959+W33dw3y@users.noreply.github.com>
2024-06-12 10:03:11 +0200

    erreur corrigé liste memories quand on a pas d'amis

--- src/components/ProfileSection.vue
+++ src/components/ProfileSection.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import type UsersResponse from '@/pocketbase-types';
+import ProfileIcon from '@/components/ProfileIcon.vue';
 
 const props = defineProps<UsersResponse>();
 
+const moodList = await pb.collection('mood').getFullList({
+    filter : `user = '${pb.authStore.model?.id}'`,
+    sort: '-created'
+});
 </script>
 
 <template>
-
+    <ProfileIcon :currentMood="moodList[0].mood"/>
     <h1>{{ name }}</h1>
+    <p>Description</p>
 </template>
