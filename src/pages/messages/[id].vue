85416c9792a08921bc7539db33d687bbbfe32d30
W33dw3y <150056959+W33dw3y@users.noreply.github.com>
2024-06-12 19:34:23 +0200

    importation des donnés aux [id] reussi

--- src/pages/messages/[id].vue
+++ src/pages/messages/[id].vue
@@ -4,17 +4,15 @@ import type { UsersResponse } from '@/pocketbase-types'
 import { ref } from 'vue'
 import { pb } from '@/backend';
 
-
-const props = defineProps<UsersResponse>();
-
 const route = useRoute('/messages/[id]');
+
+const user = await pb.collection('users').getOne(route.params.id);
 
 </script>
 
 
 <template>
     <div class="w-full flex flex-col items-center" v-scroll-lock="false">
-        <h1>Ajouter cet ami</h1>
-        {{ props.name }}
+        <h1 class="font-bold text-xl p-4 bg-red-200">Ajouter cet ami : {{ user.name }}</h1>
     </div>
 </template>
