d2b4176fc3499ff4e91c5da0d63034a4a2844cc3
W33dw3y <150056959+W33dw3y@users.noreply.github.com>
2024-06-12 09:43:24 +0200

    affichage des amis dans le profil

--- src/pages/profil/index.vue
+++ src/pages/profil/index.vue
@@ -4,12 +4,16 @@ import ProfileIcon from '@/components/ProfileIcon.vue';
 import ButtonAdd from '@/components/ButtonAdd.vue';
 import IconPen from '@/components/icons/IconPen.vue';
 import Button from '@/components/Button.vue';
+import ImgPb from '@/components/ImgPb.vue';
 
 import { pb } from '@/backend';
+import type { UsersResponse } from '@/pocketbase-types';
+import { ref } from 'vue';
 
 //Varibale pour savoir si on est sur la page des moods ou des memories
 import { isActive } from '@/components/HeaderPage.vue';
 
+const profileMode = ref(false);
 
 const moodList = await pb.collection('mood').getFullList({
         filter : `user = '${pb.authStore.model?.id}'`,
@@ -21,8 +25,15 @@ if (isActive.value == false) {
     isActive.value = true;
 }
 
-const currentUser = pb.authStore.model;
+// const currentUser = pb.authStore.model;
+const currentUser: UsersResponse[] = await pb.collection('users').getFullList({
+        filter: `id = '${pb.authStore.model?.id}'`,
+        expand: 'friends'
+    });
 console.log("currentuser", currentUser);
+
+const friendList: UsersResponse = (currentUser[0].expand as any);
+console.log(friendList.friends)
 </script>
 
 <template>
@@ -34,22 +45,36 @@ console.log("currentuser", currentUser);
                     <ProfileIcon :currentMood="moodList[0].mood"/>
                 </div>
                 <div class="*:text-center">
-                    <p class="text-2xl font-regular"> {{ currentUser?.name }}</p>
-                    <p class="text-grayDark tracking-wider">{{ currentUser?.username }}</p>
+                    <p class="text-2xl font-regular"> {{ currentUser[0].name }}</p>
+                    <p class="text-grayDark tracking-wider">{{ currentUser[0].username }}</p>
                 </div>
             </div>
 
-            <p class="text-grayDark">
-                {{ currentUser?.bio }}</p>
+            <div class="text-grayDark min-h-20 italic w-full">
+                <p v-if="currentUser[0].bio">
+                    {{ currentUser[0].bio }}
+                </p>
+                <p v-else class="text-center">
+                    Cette utilisateur n'a pas de description
+                </p>
+            </div>
             <div class="flex flex-col gap-2">
                 <h4 class="text-2xl">Amis</h4>
-                <div class="flex justify-between">
-                    <div>
-                    <!-- liste contacts -->
+                <div class="flex">
+                    <div
+                    @click="profileMode = true"
+                    v-for="oneFriend  in friendList.friends"
+                        :key="oneFriend.id"
+                        v-bind="oneFriend">
+                        <!-- liste contacts -->
+                        <ImgPb :record="oneFriend" :filename="oneFriend.avatar" class="h-12 w-12 object-cover rounded-full" alt="photo de profil" />
                     </div>
                     <ButtonAdd/>
                 </div>
             </div>
+        </div>
+        <div v-if="profileMode === true" class="bg-red-400 h-screen w-full absolute top-0">
+
         </div>
     </section>
     <section v-if="isActive === false" v-scroll-lock="true">
@@ -68,7 +93,7 @@ console.log("currentuser", currentUser);
                         <IconPen class="fill-grayDark w-4 aspect-square"/>
                     </div>
                 </div>
-                <p class="text-grayDark bg-white rounded-lg px-4 py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua.</p>                
+                <p class="text-grayDark bg-white rounded-lg px-4 py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
             <div class="my-2">
                 <Button text="Sauvegarder" variant="default" />
