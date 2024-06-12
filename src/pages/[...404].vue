434a631bb3798f240d241f711d0baa82f4b947c7
W33dw3y <150056959+W33dw3y@users.noreply.github.com>
2024-06-12 23:38:17 +0200

    integration page 404

--- src/pages/[...404].vue
+++ src/pages/[...404].vue
@@ -1,4 +1,22 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { pb
+ } from '@/backend';
+const router = useRouter();
+</script>
 <template>
-
-    <h1>404</h1>
+    <section class="h-screen flex flex-col items-center justify-center gap-6" v-scroll-lock="true">
+        <h2 class="font-Hegante text-4xl text-mainOrange">404</h2>
+        <div class="flex flex-col items-center">
+            <p>La page que vous cherchez n'existe pas</p>
+            <span class="text-blue-500">
+                <p
+                v-if="pb.authStore.model"
+                 @click="router.push('/')">Retourner a la page d'acceuil</p>
+                 <p
+                v-else
+                 @click="router.push('/connexion')">Retourner a la page de connexion</p>
+            </span>
+        </div>
+    </section>
 </template>
